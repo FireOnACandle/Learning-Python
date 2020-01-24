8aa95a410de3f930fc9be7668afe44dbe5628ff4
Experiencer <60224515+Lunartico@users.noreply.github.com>
2020-01-24 06:14:46 +0100

    Add files via upload

---
+++ Basic Calculator (WIP).py
@@ -0,0 +1,13 @@
+print("Basic Calculating")
+print()
+
+x = int(input("Enter the first number: ")) 
+y = int(input("Enter the second number: "))
+z = int(input("Enter the factor of those two numbers: "))
+name = int(input("Enter your name: "))
+
+print()
+print("Hello ", name) 
+print()
+print("The sum of those two numbers is:", sum_xy)
+
