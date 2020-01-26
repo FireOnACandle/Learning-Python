805a70fc02b83e51f45c9d23102f78f03675a795
Experiencer <60224515+Lunartico@users.noreply.github.com>
2020-01-26 11:08:47 +0100

    Update Basic Addition.py
    Basic Addition in Python

--- Basic Addition.py
+++ Basic Addition.py
@@ -1,10 +1,24 @@
-print("Basic Addition")
-print()
-x = int(input("Enter the first number :")) 
-x1 = int(input("Enter the second number :"))
-name = "user"
-sum = x + x1 
+print ("Basic Addition")
+print() 
+
+x = int (input ("Enter the first number :"))
+x1 = int (input ("Enter the second number :"))
+sum = x + x1
 
 print()
-print("Hello", name)
-print("The sum of those two numbers is :" , sum)
+print("The sum of those two numbers is :", sum) 
+
+if (sum % 2)== 0:
+ print("Sum of those two numbers is Even")
+  
+else:
+ print("Sum of those two numbers is Odd") 
+ 
+if sum <0:
+ print("The number is negative")
+ 
+elif sum > 0:
+ print("The number is positive")
+
+else:
+ print("Sum is 0")
