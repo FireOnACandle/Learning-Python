3d851acf2dfc76b983229dc7c597dfa7736fa0a2
Experiencer <60224515+Lunartico@users.noreply.github.com>
2020-01-26 07:09:00 +0100

    Update and rename Basic Addition (WIP).py to Basic Addition.py

---
+++ Basic Addition.py
@@ -0,0 +1,10 @@
+print("Basic Addition")
+print()
+x = int(input("Enter the first number :")) 
+x1 = int(input("Enter the second number :"))
+name = "user"
+sum = x + x1 
+
+print()
+print("Hello", name)
+print("The sum of those two numbers is :" , sum)
